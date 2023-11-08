23242d34101b88ad210782c847342a5f32afe051
AliAamer04 <73921348+AliAamer04@users.noreply.github.com>
2023-11-08 21:04:31 +0100

    more classes

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL ();
     }
 
     greet() {
@@ -83,7 +84,7 @@ class Color {
     }
     innerRGB() {
         const { r, g, b } = this;
-        return `rgb ${r},${g},${b} `
+        return `rgb ${r},${g},${b}`
     }
     innerHex() {
     }
@@ -96,6 +97,48 @@ class Color {
     hex() {
         const { r, g, b } = this;
         return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)
+    }
+    calcHSL() {
+        let { r, g, b } = this;
+        //Make r,g and b fractions of 1 
+        r /= 255;
+        g /= 255;
+        b /= 255;
+
+        //Find greatest and samllest channel values 
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+
+        if (delta == 0) h = 0
+        else if (cmax == r) h = ((g - b) / delta) % 6;//between yellow & magenta
+        else if (cmax == g)
+            //green is max
+            h = (b - r) / delta + 2;
+        else
+            //blue is max
+            h = (r - g) / delta + 4;
+        h = Math.round(h * 60);
+
+        //Make negative hues positive behind 360
+        if (h < 0) h += 360;
+
+        //Calculate lightness
+        l = (cmax + cmin) / 2;
+
+        //Calculate saturation
+        s = delta == 0 ? 0 : delta / (1 - Math.abs((2 * l) - 1));
+        //Multiply l and s by 100 for percentage
+        s = + (s * 100).toFixed(1);
+        l = + (l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l,
+
+
     }
 }
 
